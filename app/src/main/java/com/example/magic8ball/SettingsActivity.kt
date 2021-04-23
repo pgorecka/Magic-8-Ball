89b1530dc9d87d3b50ad548dbfebc39bd0289bde
Vonhai <pgorecka@zoho.com>
2021-04-23 22:36:22 +0200

    settings activity and button added

---
+++ app/src/main/java/com/example/magic8ball/SettingsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.magic8ball
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
